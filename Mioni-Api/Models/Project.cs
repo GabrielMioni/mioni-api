6567d6076e5e23886a2791347f761ed222bf01fd
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-04-14 19:29:53 -0700

    Added /Models/Projects

---
+++ Mioni-Api/Models/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace Mioni_Api.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
