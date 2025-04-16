3b68831201273d5bddf9e2caa3b173b801418acf
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-04-15 19:52:12 -0700

    Registered AppDbContext in Program.cs

--- Mioni-Api/Program.cs
+++ Mioni-Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Mioni_Api.Data;
 using Mioni_Api.GraphQL.Mutations;
 using Mioni_Api.GraphQL.Queries;
 
@@ -9,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services
     .AddGraphQLServer()
