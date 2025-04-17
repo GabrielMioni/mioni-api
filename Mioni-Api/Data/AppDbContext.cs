561198a0ee906a10aa85dc623207a7f06c1849a9
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-04-16 18:22:00 -0700

    Initial migration

--- Mioni-Api/Data/AppDbContext.cs
+++ Mioni-Api/Data/AppDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace Mioni_Api.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Mioni_Api.Models;
+
+namespace Mioni_Api.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Project> Projects => Set<Project>();
     }
 }
