2e00242faaa2075b79d29881134be5a7f0cc8274
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-04-16 18:21:46 -0700

    Added DesignTimeDbContextFactory.cs

---
+++ Mioni-Api/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace Mioni_Api.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.Extensions.Configuration;
+    using System.IO;
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = configuration.GetConnectionString("Default");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
