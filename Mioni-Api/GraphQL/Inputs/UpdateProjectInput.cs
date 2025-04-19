74031cd17d0c9b39f820a94b6461ad52d1e13acc
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-04-19 10:46:47 -0700

    Added UpdateProjectInput

---
+++ Mioni-Api/GraphQL/Inputs/UpdateProjectInput.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using HotChocolate;
+
+namespace Mioni_Api.GraphQL.Inputs
+{
+    public class UpdateProjectInput
+    {
+        public int Id { get; set; }
+        public Optional<string> Title { get; set; }
+        public Optional<string> Description { get; set; }
+    }
+}
