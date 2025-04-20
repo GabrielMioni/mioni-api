1e3e867587bd296bab02011a21a2265345e5b039
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-04-19 18:02:02 -0700

    In UpdateProjectInput, Title and Description properties are Optional<string?>

--- Mioni-Api/GraphQL/Inputs/UpdateProjectInput.cs
+++ Mioni-Api/GraphQL/Inputs/UpdateProjectInput.cs
@@ -6,7 +6,7 @@ namespace Mioni_Api.GraphQL.Inputs
     public class UpdateProjectInput
     {
         public int Id { get; set; }
-        public Optional<string> Title { get; set; }
-        public Optional<string> Description { get; set; }
+        public Optional<string?> Title { get; set; }
+        public Optional<string?> Description { get; set; }
     }
 }
