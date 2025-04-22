4ad472ea9da6559c9339cd09d4d2a55cbe5a9212
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-04-21 18:17:32 -0700

    Added RestoreProjectInput

---
+++ Mioni-Api/GraphQL/Inputs/RestoreProjectInput.cs
@@ -0,0 +1,10 @@
+﻿namespace Mioni_Api.GraphQL.Inputs
+{
+    public class RestoreProjectInput
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
