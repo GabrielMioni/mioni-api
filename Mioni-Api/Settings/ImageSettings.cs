ece36d162c412a7574b7b109b967ad19d6b83bcb
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-04-29 18:19:20 -0700

    Added /Settings/ImageSettings.cs

---
+++ Mioni-Api/Settings/ImageSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace Mioni_Api.Settings
+{
+    public class ImageSettings
+    {
+        public string[] AllowedExtensions { get; set; } = new[] { ".jpg", ".jpeg", ".png" };
+
+        public int MaxHeight { get; set; } = 1600;
+        public long MaxFileSizeBytes { get; set; } = 5 * 1024 * 1024; // 5 MB
+        public int MaxWidth { get; set; } = 1600;
+
+        public int MediumHeight { get; set; } = 800;
+        public int MediumWidth { get; set; } = 800;
+
+        public int Quality { get; set; } = 75;
+
+        public int ThumbnailHeight { get; set; } = 150;
+        public int ThumbnailWidth { get; set; } = 150;
+
+        public string UploadRoot { get; set; } = "uploads/";
+    }
+}
