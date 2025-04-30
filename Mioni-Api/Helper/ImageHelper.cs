6d80ebecb3b327e84ce585c882e50ebf0f1fe783
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-04-29 18:19:58 -0700

    Added ImageHelper.cs

---
+++ Mioni-Api/Helper/ImageHelper.cs
@@ -0,0 +1,36 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+using Path = System.IO.Path;
+
+namespace Mioni_Api.Helper
+{
+    public class ImageHelper
+    {
+        public static async Task ResizeAndSaveAsync (
+            Stream inputStream,
+            string outputPath,
+            int maxWidth,
+            int maxHeight,
+            int quality = 75)
+        {
+            if (inputStream.CanSeek)
+                inputStream.Position = 0;
+
+            using var image = await Image.LoadAsync(inputStream);
+
+            image.Mutate(x => x.Resize(new ResizeOptions
+            {
+                Size = new Size(maxWidth, maxHeight),
+                Mode = ResizeMode.Max
+            }));
+
+            Directory.CreateDirectory(Path.GetDirectoryName(outputPath)!);
+
+            await image.SaveAsJpegAsync(outputPath, new JpegEncoder
+            {
+                Quality = quality
+            });
+        }
+    }
+}
