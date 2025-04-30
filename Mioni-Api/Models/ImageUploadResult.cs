a8f1b66390dc192fba85762d7eeb998702620bcb
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-04-29 19:29:07 -0700

    Added /Models/ImageUploadResult

---
+++ Mioni-Api/Models/ImageUploadResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Mioni_Api.Models
+{
+    public class ImageUploadResult
+    {
+        public required string ThumbnailUrl { get; set; }
+        public required string MediumUrl { get; set; }
+        public required string LargeUrl { get; set; }
+    }
+}
