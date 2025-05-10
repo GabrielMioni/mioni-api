554ef20aa49b693730e5f89dc58adf2df48f65cb
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-05-10 09:20:41 -0700

    Added IImageDataService

---
+++ Mioni-Api/Services/Interfaces/IImageDataService.cs
@@ -0,0 +1,18 @@
+﻿using Mioni_Api.Domain.Entities;
+
+namespace Mioni_Api.Services.Interfaces
+{
+    public interface IImageDataService
+    {
+        IQueryable<ProjectImage> GetAll();
+        Task<ProjectImage?> GetByIdAsync(int id);
+        Task<ProjectImage> CreateAsync(ProjectImage projectImage);
+        Task<ProjectImage> DeleteAndReturnImageAsync(int id);
+        Task<ProjectImage> UpdateImageAsync(
+            int id,
+            string? newTitle,
+            string? newDescription,
+            bool titleProvided,
+            bool descriptionProvided);
+    }
+}
