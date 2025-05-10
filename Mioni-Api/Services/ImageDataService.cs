cfb209d260466c58f5a51616743b427a65d9baa2
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-05-10 09:21:56 -0700

    Added ImageDataService

---
+++ Mioni-Api/Services/ImageDataService.cs
@@ -0,0 +1,33 @@
+﻿using Mioni_Api.Domain.Entities;
+using Mioni_Api.Services.Interfaces;
+
+namespace Mioni_Api.Services
+{
+    public class ImageDataService : IImageDataService
+    {
+        public Task<ProjectImage> CreateAsync(ProjectImage projectImage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProjectImage> DeleteAndReturnImageAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<ProjectImage> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProjectImage?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProjectImage> UpdateImageAsync(int id, string? newTitle, string? newDescription, bool titleProvided, bool descriptionProvided)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
