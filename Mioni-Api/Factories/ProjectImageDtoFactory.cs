0e925445674a00b4c2512b926346f68658c9bc88
Gabriel Mioni <gabriel.mioni@gmail.com>
2025-05-11 13:13:11 -0700

    Added ProjectImageDtoFactory and ProjectImageDto

---
+++ Mioni-Api/Factories/ProjectImageDtoFactory.cs
@@ -0,0 +1,25 @@
+﻿using Mioni_Api.Domain.Entities;
+using Mioni_Api.Models;
+
+namespace Mioni_Api.Factories
+{
+    public class ProjectImageDtoFactory
+    {
+        public static ProjectImageDto Create(ProjectImage entity)
+        {
+            return new ProjectImageDto
+            {
+                Id = entity.Id,
+                ProjectId = entity.ProjectId,
+                FileName = entity.FileName,
+                Subfolder = entity.Subfolder,
+                SortOrder = entity.SortOrder,
+                AltText = entity.AltText,
+                Caption = entity.Caption,
+                ThumbnailUrl = entity.ThumbnailUrl,
+                MediumUrl = entity.MediumUrl,
+                LargeUrl = entity.LargeUrl
+            };
+        }
+    }
+}
---
+++ Mioni-Api/Models/ProjectImageDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Mioni_Api.Models
+{
+    public class ProjectImageDto
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public string FileName { get; set; } = default!;
+        public string Subfolder { get; set; } = default!;
+        public int SortOrder { get; set; } = 0;
+        public string? AltText { get; set; }
+        public string? Caption { get; set; }
+
+        public string ThumbnailUrl { get; set; } = default!;
+        public string MediumUrl { get; set; } = default!;
+        public required string LargeUrl { get; set; }
+    }
+}
